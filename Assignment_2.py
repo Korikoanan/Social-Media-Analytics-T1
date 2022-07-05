b1ea3424b3443265f4f8a97450002c97c3276c73
Korikoanan <65702363+Korikoanan@users.noreply.github.com>
2022-07-05 17:59:04 +0800

    Update Assignment_2.py

--- Assignment_2.py
+++ Assignment_2.py
@@ -1,119 +1,143 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jul  4 20:43:43 2022
+Created on Tue Jul  5 15:28:16 2022
 
 @author: user
 """
-
-import re
 import tweepy
-from tweepy import OAuthHandler
 from textblob import TextBlob
-
-consumer_key = "3tb2uKZTbOa78pKtN9lFWh2mt"
-consumer_secret = "1a8yR9kMAEfc8DSGHbuzXi1cxzdI8BSWXPhCOTQOirUXu5xiE6"
-access_key = "823864824727646209-wjU9Kw8kRWNhwzVK5tOUgWQWTdFcaKF"
-access_secret = "LQqYO9PofZ9uytCHbz5nkmT31pzRRwBBC2O0KNwA103fP"
+import re
+import string
+import pandas as pd
+import csv
+import random
+import networkx as nx
+import matplotlib.pyplot as plt
+import nltk
+#nltk.download('stopwords')
+#nltk.download('punkt') #punkt tokenizer model
+#nltk.download('averaged_perceptron_tagger')
+#nltk.download('wordnet')
+#nltk.download('omw-1.4')
+from nltk.tokenize import word_tokenize
+from nltk.tag import pos_tag
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk import FreqDist, bigrams
+from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer, TfidfTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report
 
 def twitter_setup():
     #Authentication and access using keys
+    consumer_key = "3tb2uKZTbOa78pKtN9lFWh2mt"
+    consumer_secret = "1a8yR9kMAEfc8DSGHbuzXi1cxzdI8BSWXPhCOTQOirUXu5xiE6"
+    access_key = "823864824727646209-wjU9Kw8kRWNhwzVK5tOUgWQWTdFcaKF"
+    access_secret = "LQqYO9PofZ9uytCHbz5nkmT31pzRRwBBC2O0KNwA103fP"
     auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
     auth.set_access_token(access_key, access_secret)
     api = tweepy.API(auth,wait_on_rate_limit=True)
-    
     try:
         api.verify_credentials()
         print("Authentication OK")
     except:
         print("Error during authentication")
-    return api
-def clean_tweet(self, tweet):
-    check_place=[str(place) for place in twitter ['Place'] if str(place) != 'nan']
-    punc = '''!()-[]{}:;'"\,<>./?@#$%^&_-'''
-    string_place=''.join([item for item in check_place])
-    for letter in string_place:
-        if letter in punc:
-            _place=string_place.replace(letter,"")
-            #word to replace the puncuations
-def get_tweet_sentiment(self, tweet):
-        '''
-        Utility function to classify sentiment of passed tweet
-        using textblob's sentiment method
-        '''
-        # create TextBlob object of passed tweet text
-        analysis = TextBlob(self.clean_tweet(tweet))
-        # set sentiment
-        if analysis.sentiment.polarity > 0:
-            return 'positive'
-        elif analysis.sentiment.polarity == 0:
-            return 'neutral'
+    return api 
+extractor = twitter_setup()
+def keyword_tweets(api, keyword, number_of_tweets):
+    new_keyword = keyword + "-filter:retweets"
+    
+    tweets=[]
+    for status in tweepy.Cursor(api.search_tweets, q=new_keyword, lang="en").items(number_of_tweets):
+        tweets.append(status)
+    return tweets
+
+keyword_alltweets = keyword_tweets(extractor, "Xiaomi", 3)
+
+def lemmatize_sentence(tokens):
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_sentence = []
+    for word, tag in pos_tag(tokens):
+        if tag.startswith('NN'):
+            pos= 'n'
+        elif tag.startswith('VB'):
+            pos = 'v'
         else:
-            return 'negative'
-  
-def get_tweets(self, query, count = 10):
-        '''
-        Main function to fetch tweets and parse them.
-        '''
-        # empty list to store parsed tweets
-        tweets = []
-  
-        try:
-            # call twitter api to fetch tweets
-            fetched_tweets = self.api.search(q = query, count = count)
-  
-            # parsing tweets one by one
-            for tweet in fetched_tweets:
-                # empty dictionary to store required params of a tweet
-                parsed_tweet = {}
-  
-                # saving text of tweet
-                parsed_tweet['text'] = tweet.text
-                # saving sentiment of tweet
-                parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text)
-  
-                # appending parsed tweet to tweets list
-                if tweet.retweet_count > 0:
-                    # if tweet has retweets, ensure that it is appended only once
-                    if parsed_tweet not in tweets:
-                        tweets.append(parsed_tweet)
-                else:
-                    tweets.append(parsed_tweet)
-  
-            # return parsed tweets
-            return tweets
-  
-        except tweepy.TweepError as e:
-            # print error (if any)
-            print("Error : " + str(e))
-  
-def main():
-    # creating object of TwitterClient Class
-    api = twitter_setup()
-    # calling function to get tweets
-    tweets = api.get_tweets(query = 'Donald Trump', count = 200)
-  
-    # picking positive tweets from tweets
-    ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
-    # percentage of positive tweets
-    print("Positive tweets percentage: {} %".format(100*len(ptweets)/len(tweets)))
-    # picking negative tweets from tweets
-    ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative']
-    # percentage of negative tweets
-    print("Negative tweets percentage: {} %".format(100*len(ntweets)/len(tweets)))
-    # percentage of neutral tweets
-    print("Neutral tweets percentage: {} % \
-        ".format(100*(len(tweets) -(len( ntweets )+len( ptweets)))/len(tweets)))
-  
-    # printing first 5 positive tweets
-    print("\n\nPositive tweets:")
-    for tweet in ptweets[:10]:
-        print(tweet['text'])
-  
-    # printing first 5 negative tweets
-    print("\n\nNegative tweets:")
-    for tweet in ntweets[:10]:
-        print(tweet['text'])
-  
-if __name__ == "__main__":
-    # calling main function
-    main()
+            pos= 'a'
+        lemmatized_sentence.append(lemmatizer.lemmatize(word,pos))
+    return lemmatized_sentence
+def remove_noise(tweet_tokens, stop_words):
+        cleaned_tokens = []
+        for token in tweet_tokens:
+            token = re.sub('http[s]','',token)
+            token = re.sub('//t.co[A-Za=z0-9]+','',token)
+            token = re.sub('(@[A-Za-z0-9_]+)','',token)
+            if(len(token) > 3) and (token not in string.punctuation) and (token.lower() not in stop_words):
+                cleaned_tokens.append(token.lower())
+        return cleaned_tokens
+stop_words = stopwords.words('english')
+stop_words.extend (['XiaoMi','Feedback','Customer'])
+
+tweets_token=keyword_alltweets['Tweets'].apply(word_tokenize).tolist()
+
+cleaned_tokens = []
+for tokens in tweets_token:
+    rm_noise=remove_noise(tokens, stop_words)
+    lemma_tokens=lemmatize_sentence(rm_noise)
+    cleaned_tokens.append(lemma_tokens)
+def get_all_words(cleaned_tokens_list):
+    tokens=[]
+    for tokens in cleaned_tokens_list:
+        for token in tokens:
+            yield token
+tokens_flat=get_all_words(cleaned_tokens)
+freq_dist = FreqDist(tokens_flat)
+print(freq_dist.most_common(20))
+
+bigram_list=[list(bigrams(tweet)) for tweet in cleaned_tokens]
+bigrams_flat=get_all_words(bigram_list)
+freq_dist_bigrams=FreqDist(bigrams_flat)
+print(freq_dist_bigrams.most_common(10))
+
+network_token_df = pd.DataFrame(freq_dist_bigrams.most_common(50), columns=['token','count'])
+
+bigrams_d=network_token_df.set_index('token').T.to_dict('records')
+
+network_graph = nx.Graph()
+
+for k, v in bigrams_d[0].items():
+    network_graph.add_edge(k[0], k[1], weight=(v* 10))
+fig, ax = plt.subplots(figsize = (20,17))
+pos = nx.spring_layout(network_graph, k=1)
+
+nx.draw_networkx(network_graph, pos,
+                 font_size =20,
+                 width=3,
+                 node_size=50,
+                 edge_color='grey',
+                 node_color='blue',
+                 with_labels = True,
+                 ax=ax)
+
+text_blob = []
+for tweet in keyword_alltweets ['Tweets'].tolist():
+    analysis = TextBlob(tweet)
+    if analysis.sentiment.polarity==0:
+        sentiment="Neutral"
+    elif analysis.sentiment.polarity>0:
+        sentiment="Positive"
+    elif analysis.sentiment.polarity<0:
+        sentiment="Negative"
+    text_blob.append(sentiment)
+keyword_alltweets['Sentiment'] = text_blob
+
+labelled_tweets=keyword_alltweets[['Tweets','Sentiment']]
+labelled_tweets.drop(labelled_tweets.loc[labelled_tweets['Sentiment']=='Neutral'].index, inplace=True)
+
+tweets_token=labelled_tweets['Tweets'].apply(word_tokenize).tolist()
+cleaned_tokens = []
+for tokens in tweets_token:
+    rm_noise=remove_noise(tokens, stop_words)
+    lemma_tokens=lemmatize_sentence(rm_noise)
+    cleaned_tokens.append(lemma_tokens)
